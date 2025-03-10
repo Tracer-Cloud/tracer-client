8a08d434b36338ec5a615b7afff7e254671e142c
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-03-10 15:14:03 +0100

    add new columns

--- migrations/20250306160500_update_batch_jobs_logs_table_with_new_fields.sql
+++ migrations/20250306160500_update_batch_jobs_logs_table_with_new_fields.sql
@@ -1,1 +1,6 @@
 -- Add migration script here
+ALTER TABLE batch_jobs_logs
+ADD COLUMN pipeline_name varchar(255),
+ADD COLUMN run_name varchar(255),
+ADD COLUMN run_id varchar(255),
+ADD COLUMN nextflow_session_uuid varchar(255);
