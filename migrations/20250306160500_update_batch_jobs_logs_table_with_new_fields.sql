6a50222498a7cf0cba790adc2b4329ede6393a03
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-03-14 14:29:18 +0100

    Delete 20250306160500_update_batch_jobs_logs_table_with_new_fields.sql

--- migrations/20250306160500_update_batch_jobs_logs_table_with_new_fields.sql
+++
@@ -1,6 +0,0 @@
--- Add migration script here
-ALTER TABLE if exists batch_jobs_logs
-ADD COLUMN if not exists pipeline_name varchar(255),
-ADD COLUMN if not exists run_name varchar(255),
-ADD COLUMN if not exists run_id varchar(255),
-ADD COLUMN if not exists nextflow_session_uuid varchar(255);
