14175dc01baa498c84827839310401ccdeb85519
David Onuh <davidonuh1@gmail.com>
2025-04-03 15:00:25 +0100

    Tag Enforcement Setup on Tracer Init (#74)
    * add pipeline tags to tracer init
    
    * update readme and compose start ups

--- src/types/cli/mod.rs
+++ src/types/cli/mod.rs
@@ -11,7 +11,46 @@ pub struct TracerCliInitArgs {
     #[clap(long)]
     pub run_id: Option<String>,
 
-    /// attribution: used to assign tags to a certain pipeline
+    #[clap(flatten)]
+    pub tags: PipelineTags,
+}
+
+#[derive(Args, Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct PipelineTags {
+    /// Enviroment: Execution Context. E.g: ci-cd, sandbox, local
+    #[clap(long, short)]
+    pub environment: String,
+
+    /// pipeline type: Used to identify the type of the pipeline.
+    #[clap(long)]
+    pub pipeline_type: String,
+
+    /// User Operator: Responsible individual executing the pipeline
+    #[clap(long)]
+    pub user_operator: String,
+
+    /// Department: Organizational unit, e.g., "Research"
+    #[clap(long, default_value = "Research")]
+    pub department: String,
+
+    /// Team: Business division, e.g., "Oncology"
+    #[clap(long, default_value = "Oncology")]
+    pub team: String,
+
+    /// Others: Any other tag you'd like to specify
     #[clap(long, value_delimiter = ',')]
-    pub tags: Vec<String>,
+    pub others: Vec<String>,
+}
+
+impl Default for PipelineTags {
+    fn default() -> Self {
+        Self {
+            environment: "local".into(),
+            pipeline_type: "generic".into(),
+            user_operator: "tracer_user".into(),
+            department: "dev".into(),
+            team: "dev".into(),
+            others: vec![],
+        }
+    }
 }
