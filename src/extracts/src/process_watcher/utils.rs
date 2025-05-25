890f7145943d36fb17c8623369e73cdc915e9fb8
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-05-25 19:24:35 +0200

    fixed type names

--- src/extracts/src/process_watcher/utils.rs
+++ src/extracts/src/process_watcher/utils.rs
@@ -2,19 +2,19 @@ use super::ProcessState;
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::RwLock;
-use tracer_common::types::trigger::{ExitReason, FinishTrigger, OomTrigger};
+use tracer_common::types::trigger::{ExitReason, OutOfMemoryTrigger, ProcessEndTrigger};
 use tracing::debug;
 
 /// Enriches finish triggers with OOM reason if they were OOM victims
 pub async fn handle_oom_terminations(
     state: &Arc<RwLock<ProcessState>>,
-    finish_triggers: &mut [FinishTrigger],
+    finish_triggers: &mut [ProcessEndTrigger],
 ) {
     let mut state = state.write().await;
 
     for finish in finish_triggers.iter_mut() {
         if state.oom_victims.remove(&finish.pid).is_some() {
-            finish.exit_reason = Some(ExitReason::OomKilled);
+            finish.exit_reason = Some(ExitReason::OutOfMemoryKilled);
             debug!("Marked PID {} as OOM-killed", finish.pid);
         }
     }
@@ -22,8 +22,8 @@ pub async fn handle_oom_terminations(
 
 pub async fn handle_oom_signals(
     state: &Arc<RwLock<ProcessState>>,
-    triggers: Vec<OomTrigger>,
-) -> HashMap<usize, OomTrigger> {
+    triggers: Vec<OutOfMemoryTrigger>,
+) -> HashMap<usize, OutOfMemoryTrigger> {
     let mut victims = HashMap::new();
     let mut state = state.write().await;
 
