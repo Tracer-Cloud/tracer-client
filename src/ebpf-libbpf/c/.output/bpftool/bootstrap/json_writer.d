97d91256298d604d33798d1a6ebf9df7143d38af
Vaibhav Upreti <vaibhavupreti@users.noreply.github.com>
2025-05-28 00:34:32 +0000

    fix: revert ebpf-libpf changes

--- src/ebpf-libbpf/c/.output/bpftool/bootstrap/json_writer.d
+++
@@ -1,5 +0,0 @@
-/workspaces/tracer-client/src/ebpf-libbpf/c/.output/bpftool/bootstrap/json_writer.o: \
- json_writer.c json_writer.h \
- /workspaces/tracer-client/vendor/bpftool/include/linux/compiler.h \
- /workspaces/tracer-client/vendor/bpftool/include/linux/compiler_types.h \
- /workspaces/tracer-client/vendor/bpftool/include/linux/compiler-gcc.h
