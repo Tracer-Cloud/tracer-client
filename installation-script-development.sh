a9170396f4d2d6a8ed5df3eabbd7522a980cc1a2
vincenthus <33206282+davincios@users.noreply.github.com>
2025-06-03 23:52:56 +0300

    File Cleanup (#245)
    * Add NextFlow process matching based on regex patterns
    
    - Introduced `nf_process_match.rs` for matching commands against defined processes using regex patterns.
    - Added a new JSON file `nf_process_list.json` containing process definitions and test commands.
    
    * moving the process pipeline
    
    * removed legacy files
    
    * development parameter
    
    ---------
    
    Co-authored-by: ashtonsix <me@ashtonsix.com>

--- installation-script-development.sh
+++ installation-script-development.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Development installation script
-# This script downloads and executes the main installation script with production parameter
+# This script downloads and executes the main installation script with development parameter
 
 # URL to the main installation script
 INSTALL_SCRIPT_URL="https://raw.githubusercontent.com/Tracer-Cloud/tracer-client/refs/heads/dev/installation.sh"
 
-# Download and execute the installation script with production parameter
+# Download and execute the installation script with development parameter
 curl -sSL "$INSTALL_SCRIPT_URL" | bash -s development
 
