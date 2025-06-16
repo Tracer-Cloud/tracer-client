db2a85b12e16bf96cfb064b2dd0ad25cf596b3c7
David Onuh <davidonuh1@gmail.com>
2025-06-16 10:14:17 +0300

    Persisting TRACER User ID into the script (#291)
    * remove user_id from sub layer of script caller
    
    * temp remove suppressed statement on installation script
    
    * temp point to staging link for testing
    
    * tested out implementation.. reverting to main installation url

--- installation-script-development.sh
+++ installation-script-development.sh
@@ -5,7 +5,6 @@
 
 # URL to the main installation script
 INSTALL_SCRIPT_URL="https://install.tracer.cloud/installation.sh"
-
 # Get the branch name from the first argument, default to "development"
 BRANCH_NAME=${1:-"development"}
 
