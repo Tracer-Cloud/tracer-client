c1476627d4f75e76fd111a83d7ed6caebd6a63f8
David Onuh <davidonuh1@gmail.com>
2025-06-16 10:06:04 +0300

    temp point to staging link for testing

--- installation-script-development.sh
+++ installation-script-development.sh
@@ -4,8 +4,8 @@
 # This script downloads and executes the main installation script with branch + user ID
 
 # URL to the main installation script
-INSTALL_SCRIPT_URL="https://install.tracer.cloud/installation.sh"
-
+# INSTALL_SCRIPT_URL="https://install.tracer.cloud/installation.sh"
+INSTALL_SCRIPT_URL="https://persist-tracer-user-id.tracer-client.pages.dev/installation.sh"
 # Get the branch name from the first argument, default to "development"
 BRANCH_NAME=${1:-"development"}
 
--- installation-script.sh
+++ installation-script.sh
@@ -4,8 +4,8 @@
 # This script downloads and executes the main installation script with production parameter
 
 # URL to the main installation script
-INSTALL_SCRIPT_URL="https://install.tracer.cloud/installation.sh"
-
+# INSTALL_SCRIPT_URL="https://install.tracer.cloud/installation.sh"
+INSTALL_SCRIPT_URL="https://persist-tracer-user-id.tracer-client.pages.dev/installation.sh"
 
 # Download and execute the installation script with production parameter
 curl -sSL "$INSTALL_SCRIPT_URL" | bash -s production
