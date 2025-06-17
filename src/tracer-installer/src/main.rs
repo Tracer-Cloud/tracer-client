2bed212e5e1c985ed6fbbdde53bd34f467aa4212
David Onuh <davidonuh1@gmail.com>
2025-06-17 13:32:01 +0300

    create url builder for platform types

--- src/tracer-installer/src/main.rs
+++ src/tracer-installer/src/main.rs
@@ -1,10 +1,19 @@
 use checks::CheckManager;
+use installer::run_installer;
 
 mod checks;
+mod installer;
+mod types;
 
 #[tokio::main]
 async fn main() {
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("Failed to install rustls crypto provider");
+
     println!("Welcome to tracer rust installer");
     let requirements = CheckManager::new();
     requirements.run_all().await;
+
+    run_installer().await;
 }
