8a89f583952246134d4eaec7dc199afde2a6a388
davincios <jvf.hus@gmail.com>
2025-06-17 02:19:27 +0100

    process manager refactoring

--- src/tracer/src/extracts/process/process_manager/mod.rs
+++ src/tracer/src/extracts/process/process_manager/mod.rs
@@ -1,10 +1,10 @@
 pub mod handlers;
 pub mod logger;
 pub mod matcher;
-pub mod process_manager;
+pub mod manager;
 pub mod metrics;
 pub mod state;
 pub mod system_refresher;
 
 // Re-export the main ProcessManager for convenience
-pub use process_manager::ProcessManager;
+pub use manager::ProcessManager;
