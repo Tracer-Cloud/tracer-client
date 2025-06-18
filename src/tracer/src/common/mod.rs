d3bc0aea994154bfe08afc0f8470e90d0ff5d6fe
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-06-18 22:21:12 +0100

    update target matcher

--- src/tracer/src/common/mod.rs
+++ src/tracer/src/common/mod.rs
@@ -1,3 +1,4 @@
+pub mod recorder; // todo: this is very ugly, please move me out to tracer client
 pub mod constants;
 pub mod debug_log;
 pub mod recorder; // todo: this is very ugly, please move me out to tracer client
--- src/tracer/src/extracts/process/process_manager/state.rs
+++ src/tracer/src/extracts/process/process_manager/state.rs
@@ -1,5 +1,5 @@
-use crate::common::target_process::target::Target;
 use crate::common::target_process::target_manager::TargetManager;
+use crate::common::target_process::Target;
 use crate::extracts::process::types::process_state::ProcessState;
 use anyhow::Result;
 use std::collections::{HashMap, HashSet};
