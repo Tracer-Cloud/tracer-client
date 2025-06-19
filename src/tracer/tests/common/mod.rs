f6c6e29d4f04db2190df7ab566cb835450fd7293
jdidion <github@didion.net>
2025-06-18 19:20:32 -0700

    rename test (pipeline-nfcore-rnaseq.test.rs is invalid), bugfixes

--- src/tracer/tests/common/mod.rs
+++ src/tracer/tests/common/mod.rs
@@ -4,18 +4,19 @@ pub use nf_process_match::{NextFlowProcessMatcher, ProcessInfo};
 
 use chrono::Utc;
 use tracer_ebpf::ebpf_trigger::ProcessStartTrigger;
+use tracer_ebpf::ebpf_trigger::Trigger;
 
 pub const DUMMY_PID: usize = 1;
 pub const DUMMY_PPID: usize = 0;
 pub const PATH_SEPARATOR: &str = "/";
 
-pub fn new_process_start_trigger(cmd: &str, path: &str) -> ProcessStartTrigger {
-    ProcessStartTrigger {
+pub fn new_process_start_trigger(cmd: &str, path: &str) -> Trigger {
+    Trigger::ProcessStart(ProcessStartTrigger {
         pid: DUMMY_PID,
         ppid: DUMMY_PPID,
         comm: path.split("/").last().unwrap().to_string(),
         argv: cmd.split_whitespace().map(String::from).collect(),
         file_name: path.to_string(),
         started_at: Utc::now(),
-    }
+    })
 }
