c075bf98968c99215a761c133544507f19ec6d91
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-06-21 01:35:30 +0200

    fix folder path'

--- src/tracer/build.rs
+++ src/tracer/build.rs
@@ -1,4 +1,4 @@
 fn main() {
     // useful to trigger changes in the cargo build on the json file
-    println!("cargo:rerun-if-changed=src/common/target_process/yaml_rules/tracer.rules.yml");
+    println!("cargo:rerun-if-changed=src/common/target_process/yml_rules/tracer.rules.yml");
 }
