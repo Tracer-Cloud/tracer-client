14e58117b12ee040705cc1bb19d5f70bcaa20136
John Didion <github@didion.net>
2025-06-23 10:44:28 -0700

    Add git hash and dirty repo info to version (#332)
    Generate file with build-time info and use it to construct the version, rather than modifying the version during the release workflow.

--- src/tracer/src/utils/mod.rs
+++ src/tracer/src/utils/mod.rs
@@ -1,7 +1,7 @@
 mod info_formatter;
 pub use info_formatter::InfoFormatter;
 mod version;
-pub use version::Version;
+pub use version::{FullVersion, Version};
 mod sentry;
 pub use sentry::Sentry;
 pub mod analytics;
