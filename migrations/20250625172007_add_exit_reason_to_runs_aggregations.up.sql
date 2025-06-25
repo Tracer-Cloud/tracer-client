f83bdc26b897b2fd74a05df86bdd31712f7ebfc5
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-06-25 19:29:08 +0200

    add exit reason field

--- migrations/20250625172007_add_exit_reason_to_runs_aggregations.up.sql
+++ migrations/20250625172007_add_exit_reason_to_runs_aggregations.up.sql
@@ -1,1 +1,131 @@
 -- Add up migration script here
+-- adding new column for the exit_reason
+alter table runs_aggregations add column exit_reasons text;
+
+CREATE OR REPLACE FUNCTION update_runs_aggregation()
+RETURNS TRIGGER AS $$
+DECLARE
+    exit_reason_value TEXT;
+BEGIN
+    IF NEW.process_status = 'new_run' THEN
+        -- Create a new record
+        INSERT INTO runs_aggregations (
+            trace_id,
+            run_id,
+            pipeline_name,
+            status,
+            total_runtime_sec,
+            start_time,
+            end_time,
+            max_ram,
+            avg_ram,
+            max_cpu,
+            ec2_cost_per_hour,
+            ec2_cost_per_second,
+            system_ram_total,
+            system_metrics_events_count,
+            total_datasets,
+            total_cost,
+            max_ram_percent,
+            avg_ram_percent,
+            system_cpu_cores,
+            tags,
+            run_name,
+            exit_reasons
+        ) VALUES (
+            new.trace_id,
+            new.run_id,
+            new.pipeline_name,
+            'Running',
+            0,
+            new.timestamp,
+            null,
+            0,
+            0,
+            0,
+            new.ec2_cost_per_hour,
+            new.ec2_cost_per_hour/3600,
+            COALESCE((new.resource_attributes->>'system_properties.total_memory')::double precision, 0),
+            0,
+            0,
+            0,
+            0,
+            0,
+            COALESCE((new.resource_attributes->>'system_properties.num_cpus')::integer, 0),
+            new.tags,
+            new.run_name,
+            ''
+        )
+        ON CONFLICT (trace_id, run_id) DO NOTHING; -- Avoid duplication if exists already
+
+    -- will work after ebpf datasets detection
+    ELSIF NEW.process_status = 'dataset_opened' THEN
+        -- Increment datasets
+        UPDATE runs_aggregations
+        SET total_datasets = total_datasets + 1
+        WHERE trace_id = NEW.trace_id;
+
+    -- will work after ebpf
+    ELSIF NEW.process_status = 'pipeline_terminated' THEN
+        -- Mark complete and add to runtime
+        UPDATE runs_aggregations
+        SET
+            status = 'Complete',
+            end_time = NEW.timestamp
+        WHERE trace_id = NEW.trace_id;
+
+    ELSIF NEW.process_status = 'metric_event' THEN
+            -- Update RAM, CPU, AVG_CPU and total_metrics_events
+        UPDATE runs_aggregations
+        SET
+            max_ram = GREATEST(max_ram, COALESCE(NEW.mem_used, 0)),
+            max_cpu = GREATEST(max_cpu, COALESCE(NEW.cpu_usage, 0)),
+            end_time = new.timestamp,
+            system_metrics_events_count = system_metrics_events_count + 1,
+            avg_ram = (avg_ram * system_metrics_events_count + NEW.mem_used)
+                / (system_metrics_events_count + 1),
+            total_cost = EXTRACT(EPOCH FROM (NEW.timestamp - start_time)) * ec2_cost_per_second,
+            -- calculate max ram % and avg ram %
+            max_ram_percent = GREATEST(max_ram_percent, COALESCE(NEW.mem_used, 0) / NULLIF(system_ram_total, 0) * 100),
+            avg_ram_percent = ((avg_ram * system_metrics_events_count + NEW.mem_used) / (system_metrics_events_count + 1)) / NULLIF(system_ram_total, 0) * 100
+        -- add the disk updates as well
+        WHERE trace_id = NEW.trace_id;
+
+    ELSIF NEW.process_status = 'finished_tool_execution' THEN
+        -- Extract and append exit reason
+        IF NEW.attributes ? 'completed_process.exit_reason' THEN
+            exit_reason_value := TRIM(NEW.attributes ->> 'completed_process.exit_reason');
+
+            -- Only proceed if exit_reason_value is not empty
+            IF exit_reason_value != '' THEN
+                UPDATE runs_aggregations
+                SET exit_reasons = CASE
+                    WHEN exit_reasons IS NULL OR exit_reasons = '' THEN
+                        exit_reason_value
+                    WHEN exit_reasons NOT LIKE '%' || exit_reason_value || '%' THEN
+                        exit_reasons || ', ' || exit_reason_value
+                    ELSE
+                        exit_reasons  -- Don't add duplicates
+                END
+                WHERE trace_id = NEW.trace_id;
+            END IF;
+        END IF;
+
+    END IF;
+
+    UPDATE runs_aggregations
+    SET
+        total_runtime_sec = EXTRACT(EPOCH FROM (NEW.timestamp - start_time))
+    WHERE trace_id = NEW.trace_id;
+
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Now attach the trigger to the events table
+DROP TRIGGER IF EXISTS trigger_update_runs_aggregation ON batch_jobs_logs;
+
+CREATE TRIGGER trigger_update_runs_aggregation
+    AFTER INSERT ON batch_jobs_logs
+    FOR EACH ROW
+    EXECUTE FUNCTION update_runs_aggregation();
