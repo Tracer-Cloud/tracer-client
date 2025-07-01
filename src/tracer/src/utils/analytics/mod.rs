7c864b7989851e722d0989c174c6db3239e2a7cf
mountwo23 <55633208+mountwo23@users.noreply.github.com>
2025-07-01 13:53:45 +0100

    Import tracer-installer-emit-event

--- src/tracer/src/utils/analytics/mod.rs
+++ src/tracer/src/utils/analytics/mod.rs
@@ -4,21 +4,24 @@ use crate::constants::TRACER_ANALYTICS_ENDPOINT;
 use crate::utils::analytics::types::{AnalyticsEventType, AnalyticsPayload};
 use reqwest::Client;
 use std::collections::HashMap;
+use tokio_retry::strategy::{jitter, ExponentialBackoff};
+use tokio_retry::Retry;
 
 pub fn spawn_event(
     explicit_user_id: Option<String>,
     event: AnalyticsEventType,
     metadata: Option<HashMap<String, String>>,
 ) {
-    tokio::spawn(emit_event(explicit_user_id, event, metadata));
+    tokio::spawn(send_event(explicit_user_id, event, metadata));
 }
 
-async fn emit_event(
-    explicit_user_id: Option<String>,
+pub async fn send_event(
+    user_id: Option<String>,
     event: AnalyticsEventType,
     metadata: Option<HashMap<String, String>>,
 ) -> anyhow::Result<()> {
-    let user_id = match explicit_user_id {
+    let client = Client::new();
+    let user_id = match user_id {
         Some(id) => id,
         None => match std::env::var("TRACER_USER_ID") {
             Ok(val) if !val.trim().is_empty() => val,
@@ -26,27 +29,31 @@ async fn emit_event(
         },
     };
 
+    let retry_strategy = ExponentialBackoff::from_millis(500).map(jitter).take(3);
+
     let payload = AnalyticsPayload {
-        user_id: &user_id,
+        user_id: user_id.as_str(),
         event_name: event.as_str(),
         metadata,
     };
+    Retry::spawn(retry_strategy, || async {
+        let res = client
+            .post(TRACER_ANALYTICS_ENDPOINT)
+            .json(&payload)
+            .send()
+            .await?;
 
-    let client = Client::new();
-    let res = client
-        .post(TRACER_ANALYTICS_ENDPOINT)
-        .header("Content-Type", "application/json")
-        .json(&payload)
-        .send()
-        .await?;
+        if res.status().is_success() {
+            Ok(())
+        } else {
+            eprintln!(
+                "⚠️  Failed to send analytics event: {} [{}]",
+                event.as_str(),
+                res.status()
+            );
 
-    if !res.status().is_success() {
-        tracing::error!(
-            "Failed to send analytics event {:?} (status: {})",
-            event,
-            res.status()
-        );
-    }
-
-    Ok(())
+            Err(anyhow::anyhow!("status = {}", res.status()))
+        }
+    })
+    .await
 }
