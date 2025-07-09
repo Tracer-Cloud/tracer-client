d6d42126f3daf051cb070fb93ca7c534db63cd0a
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-07-09 12:53:59 +0100

    removed more fields

--- migrations/20250709111124_remove_fields_from_database.down.sql
+++ migrations/20250709111124_remove_fields_from_database.down.sql
@@ -1,5 +1,5 @@
 -- Add down migration script here
-alter table batch_jobs_logs
+alter table events
 add column if not exists source_type TEXT,
 add column if not exists instrumentation_version TEXT,
 add column if not exists job_id TEXT,
@@ -7,7 +7,9 @@ add column if not exists parent_job_id TEXT,
 add column if not exists child_job_ids TEXT[],
 add column if not exists workflow_engine TEXT,
 add column if not exists event_type TEXT,
-add column if not exists process_type TEXT;
+add column if not exists process_type TEXT,
+add column if not exists severity_text TEXT,
+add column if not exists severity_number INT;
 
 alter table tools_events
 add column if not exists source_type TEXT,
@@ -17,6 +19,9 @@ add column if not exists parent_job_id TEXT,
 add column if not exists child_job_ids TEXT[],
 add column if not exists workflow_engine TEXT,
 add column if not exists event_type TEXT,
-add column if not exists process_type TEXT;
+add column if not exists process_type TEXT,
+add column if not exists severity_text TEXT,
+add column if not exists severity_number INT;
+
 
 ALTER TABLE events RENAME TO batch_jobs_logs
--- migrations/20250709111124_remove_fields_from_database.up.sql
+++ migrations/20250709111124_remove_fields_from_database.up.sql
@@ -8,7 +8,9 @@ drop column if exists parent_job_id,
 drop column if exists child_job_ids,
 drop column if exists workflow_engine,
 drop column if exists event_type,
-drop column if exists process_type;
+drop column if exists process_type,
+drop column if exists severity_text,
+drop column if exists severity_number;
 
 alter table tools_events
 drop column if exists source_type,
@@ -18,7 +20,9 @@ drop column if exists parent_job_id,
 drop column if exists child_job_ids,
 drop column if exists workflow_engine,
 drop column if exists event_type,
-drop column if exists process_type;
+drop column if exists process_type,
+drop column if exists severity_text,
+drop column if exists severity_number;
 
 -- renaming batch_jobs_logs table to events as makes more sense
 ALTER TABLE batch_jobs_logs RENAME TO events
