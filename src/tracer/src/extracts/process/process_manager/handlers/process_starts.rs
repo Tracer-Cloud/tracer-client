1bcb12f687f4b7b04cd3188b27074be82eb11add
David Onuh <davidonuh1@gmail.com>
2025-07-16 23:10:40 +0100

    remove prefiltering rules for now

--- src/tracer/src/extracts/process/process_manager/handlers/process_starts.rs
+++ src/tracer/src/extracts/process/process_manager/handlers/process_starts.rs
@@ -1,10 +1,7 @@
+use crate::extracts::process::process_manager::logger::ProcessLogger;
 use crate::extracts::process::process_manager::matcher::Filter;
 use crate::extracts::process::process_manager::state::StateManager;
 use crate::extracts::process::process_manager::system_refresher::SystemRefresher;
-use crate::{
-    constants::PROCESS_POLLING_INTERVAL_MS,
-    extracts::process::process_manager::logger::ProcessLogger,
-};
 use anyhow::Result;
 use std::collections::{HashMap, HashSet};
 use tracer_ebpf::ebpf_trigger::ProcessStartTrigger;
@@ -22,30 +19,11 @@ impl ProcessStartHandler {
         system_refresher: &SystemRefresher,
         triggers: Vec<ProcessStartTrigger>,
     ) -> Result<()> {
-        let total_triggers = triggers.len();
-        debug!("Handling {} process start triggers", total_triggers);
+        debug!("Handling {} process start triggers", triggers.len());
 
-        let existing = {
-            let guard = state_manager.get_state().await;
-            guard.get_processes().clone()
-        };
+        Self::store_triggers(state_manager, triggers.clone()).await;
 
-        let unique_triggers = Self::filter_unique_triggers(
-            triggers,
-            existing.values().cloned(),
-            PROCESS_POLLING_INTERVAL_MS as i64,
-        );
-
-        debug!(
-            "Filtered {} duplicates; proceeding with {} unique triggers",
-            total_triggers - unique_triggers.len(),
-            unique_triggers.len()
-        );
-
-        Self::store_triggers(state_manager, unique_triggers.clone()).await;
-
-        let matched_processes =
-            Self::match_processes(state_manager, matcher, unique_triggers).await;
+        let matched_processes = Self::match_processes(state_manager, matcher, triggers).await;
 
         if matched_processes.is_empty() {
             debug!("No matching processes found; exiting early.");
@@ -137,7 +115,7 @@ impl ProcessStartHandler {
         state_manager.update_monitoring(matched_processes).await
     }
 
-    fn filter_unique_triggers(
+    fn _filter_unique_triggers(
         incoming: Vec<ProcessStartTrigger>,
         mut existing: impl Iterator<Item = ProcessStartTrigger>,
         max_ms_drift: i64,
