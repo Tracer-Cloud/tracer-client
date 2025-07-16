79c8b3c207dd9a121b1e39c429f9c0de9915ceeb
David Onuh <davidonuh1@gmail.com>
2025-07-16 12:54:51 +0100

    fix imports after rebasing

--- src/tracer/src/cli/handlers/mod.rs
+++ src/tracer/src/cli/handlers/mod.rs
@@ -1,8 +1,10 @@
 mod info;
 mod init;
+mod uninstall;
 mod update;
 
 pub(super) use info::info;
 pub use init::arguments;
 pub(super) use init::init;
+pub(super) use uninstall::uninstall;
 pub(super) use update::update;
