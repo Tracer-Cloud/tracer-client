145c48128da0a2021be7b70cc945deb6bbb9d654
davincios <jvf.hus@gmail.com>
2025-07-22 22:39:02 +0100

    changing run url

--- src/tracer/src/constants.rs
+++ src/tracer/src/constants.rs
@@ -11,7 +11,7 @@ pub const FILE_SIZE_NOT_CHANGING_PERIOD_MS: u64 = 1000 * 60;
 pub const LOG_FORWARD_ENDPOINT_DEV: &str = "https://sandbox.tracer.cloud/api/logs-forward/dev";
 pub const LOG_FORWARD_ENDPOINT_PROD: &str = "https://sandbox.tracer.cloud/api/logs-forward/prod";
 pub const SENTRY_DSN: &str = "https://35e0843e6748d2c93dfd56716f2eecfe@o4509281671380992.ingest.us.sentry.io/4509281680949248";
-pub const DASHBOARD_BASE: &str = "https://sandbox.tracer.cloud/run-overview";
+pub const DASHBOARD_BASE: &str = "https://dev.sandbox.tracer.cloud/run-overview";
 // pub const SENTRY_DSN: &str = "https://add417a1c944b1b2110b4f3ea8d7fbea@o4509525906948096.ingest.de.sentry.io/4509530452328528"; // todo remove - used for testing new alerts
 
 pub const AWS_REGION: AwsRegion = UsEast2;
