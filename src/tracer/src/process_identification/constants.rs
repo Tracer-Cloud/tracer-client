5f84b669f8244cb0896bf1c3a3846b91faa538f0
mountwo23 <55633208+mountwo23@users.noreply.github.com>
2025-07-23 16:43:08 +0100

    Update constants.rs

--- src/tracer/src/process_identification/constants.rs
+++ src/tracer/src/process_identification/constants.rs
@@ -3,10 +3,4 @@ pub const PID_FILE: &str = "/tmp/tracer/tracerd.pid";
 pub const STDOUT_FILE: &str = "/tmp/tracer/tracerd.out";
 pub const STDERR_FILE: &str = "/tmp/tracer/tracerd.err";
 pub const LOG_FILE: &str = "/tmp/tracer/daemon.log";
-pub const DEBUG_LOG: &str = "/tmp/tracer/debug.log";
-pub const SYSLOG_FILE: &str = "/var/log/syslog";
-
-pub const REPO_OWNER: &str = "Tracer-Cloud";
-pub const REPO_NAME: &str = "tracer-client";
-
 pub const DEFAULT_DAEMON_PORT: u16 = 8722;
