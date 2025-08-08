03ee344e780f03995081b867aa9d1c59b5a10b94
jdidion <github@didion.net>
2025-08-08 11:51:52 -0700

    add warning when falling back to username

--- src/tracer/src/cli/handlers/init/arguments.rs
+++ src/tracer/src/cli/handlers/init/arguments.rs
@@ -2,6 +2,7 @@ use crate::cli::handlers::INTERACTIVE_THEME;
 use crate::process_identification::types::pipeline_tags::PipelineTags;
 use crate::utils::env;
 use crate::utils::input_validation::{get_validated_input, StringValueParser};
+use crate::warning_message;
 use clap::{Args, ValueEnum};
 use dialoguer::Select;
 use serde::Serialize;
@@ -88,7 +89,19 @@ impl TracerCliInitArgs {
             (None, PromptMode::None) => None,
         }
         // TODO: remove this once we can source the user ID from the credentials file
-        .or_else(|| env::get_env_var(USERNAME_ENV_VAR))
+        .or_else(|| {
+            let username = env::get_env_var(USERNAME_ENV_VAR);
+            if let Some(username) = &username {
+                warning_message!(
+                    "Failed to get user ID from environment variable, command line, or prompt. \
+                    defaulting to the system username {}, which may not be your Tracer user ID! \
+                    Please set the TRACER_USER_ID environment variable or specify the --user-id \
+                    option.",
+                    username
+                );
+            }
+            username
+        })
         .or_else(print_help)
         .expect("Failed to get user ID from environment variable, command line, or prompt");
         tags.user_id = Some(user_id.clone());
