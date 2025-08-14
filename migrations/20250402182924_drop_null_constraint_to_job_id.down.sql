aa0c9eb0561e698130d7d7e0913b39475cec2415
davincios <jvf.hus@gmail.com>
2025-08-14 17:33:42 +0100

    infrastructure clean up

--- migrations/20250402182924_drop_null_constraint_to_job_id.down.sql
+++
@@ -1,5 +0,0 @@
-UPDATE batch_jobs_logs SET job_id = 'default' WHERE job_id IS NULL;
-
-
-ALTER TABLE batch_jobs_logs
-    ALTER COLUMN job_id SET NOT NULL;
--- migrations/20250402182924_drop_null_constraint_to_job_id.up.sql
+++
@@ -1,3 +0,0 @@
--- Add up migration script here
-ALTER TABLE batch_jobs_logs
-    ALTER COLUMN job_id DROP NOT NULL;
