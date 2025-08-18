9c81b61f6d3658d5979fb16c5e4614d4c0c90235
davincios <jvf.hus@gmail.com>
2025-08-19 00:06:03 +0100

    moving the infrastructure folder into the bioinformatics pipelines repository

--- infrastructure/modules/grafana/outputs.tf
+++
@@ -1,9 +0,0 @@
-
-
-output "workspace_id" {
-  value = aws_grafana_workspace.tracer_workspace.id
-}
-
-output "workspace_url" {
-  value = "https://${aws_grafana_workspace.tracer_workspace.endpoint}"
-}
