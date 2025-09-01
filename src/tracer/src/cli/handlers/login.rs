72a0adc2df880bd6b83073f23539bf5f4f09544a
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-09-01 21:42:45 +0100

    add tracer login

--- src/tracer/src/cli/handlers/login.rs
+++ src/tracer/src/cli/handlers/login.rs
@@ -2,7 +2,7 @@ use crate::utils::browser::browser;
 
 pub fn login() -> Result<String, Box<dyn std::error::Error>> {
     let app_url = "http://localhost:3000";
-    let login_url = format!("{}/api/auth/cli-login", app_url);
+    let login_url = format!("{}/sign-in?cli=true", app_url);
 
     // open the browser window to login
     browser::open_url(&login_url);
