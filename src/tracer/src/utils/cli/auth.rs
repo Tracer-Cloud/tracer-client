2c2961349275d1d8fbce730fff9575814811f1b4
Michele Verriello <49948653+MicheleVerriello@users.noreply.github.com>
2025-12-02 16:14:28 +0000

    cleaned up

--- src/tracer/src/utils/cli/auth.rs
+++ src/tracer/src/utils/cli/auth.rs
@@ -5,21 +5,28 @@ use crate::constants::{
 };
 
 pub fn get_auth_url(platform: &str, auth_type: AuthType, is_development_environment: bool) -> &str {
-    if auth_type == AuthType::Login {
-        if platform.eq_ignore_ascii_case("local") {
-            CLI_LOGIN_URL_LOCAL
-        } else if platform.eq_ignore_ascii_case("dev") || is_development_environment {
-            CLI_LOGIN_URL_DEV
-        } else {
-            CLI_LOGIN_URL_PROD
-        }
+    match auth_type {
+        AuthType::Login => get_login_url(platform, is_development_environment),
+        AuthType::SignUp => get_signup_url(platform, is_development_environment),
+    }
+}
+
+fn get_login_url(platform: &str, is_development_environment: bool) -> &str {
+    if platform.eq_ignore_ascii_case("local") {
+        CLI_LOGIN_URL_LOCAL
+    } else if platform.eq_ignore_ascii_case("dev") || is_development_environment {
+        CLI_LOGIN_URL_DEV
     } else {
-        if platform.eq_ignore_ascii_case("local") {
-            CLI_SIGNUP_URL_LOCAL
-        } else if platform.eq_ignore_ascii_case("dev") || is_development_environment {
-            CLI_SIGNUP_URL_DEV
-        } else {
-            CLI_SIGNUP_URL_PROD
-        }
+        CLI_LOGIN_URL_PROD
+    }
+}
+
+fn get_signup_url(platform: &str, is_development_environment: bool) -> &str {
+    if platform.eq_ignore_ascii_case("local") {
+        CLI_SIGNUP_URL_LOCAL
+    } else if platform.eq_ignore_ascii_case("dev") || is_development_environment {
+        CLI_SIGNUP_URL_DEV
+    } else {
+        CLI_SIGNUP_URL_PROD
     }
 }
